0df80650b45305ef89b006ec7023d410c6e7953e
suimenkathemove <yoshiharu2580@gmail.com>
2025-09-25 09:07:35 +0900

    fix for upgrade version

--- parent.go
+++ parent.go
@@ -3,17 +3,18 @@ package notion
 type Parent struct {
 	Type ParentType `json:"type,omitempty"`
 
-	BlockID    string `json:"block_id,omitempty"`
-	PageID     string `json:"page_id,omitempty"`
-	DatabaseID string `json:"database_id,omitempty"`
-	Workspace  bool   `json:"workspace,omitempty"`
+	BlockID      string `json:"block_id,omitempty"`
+	PageID       string `json:"page_id,omitempty"`
+	DataSourceID string `json:"data_source_id,omitempty"`
+	Workspace    bool   `json:"workspace,omitempty"`
 }
 
 type ParentType string
 
 const (
-	ParentTypeDatabase  ParentType = "database_id"
-	ParentTypePage      ParentType = "page_id"
-	ParentTypeBlock     ParentType = "block_id"
-	ParentTypeWorkspace ParentType = "workspace"
+	ParentTypeDatabase   ParentType = "database_id"
+	ParentTypeDataSource ParentType = "data_source_id"
+	ParentTypePage       ParentType = "page_id"
+	ParentTypeBlock      ParentType = "block_id"
+	ParentTypeWorkspace  ParentType = "workspace"
 )
